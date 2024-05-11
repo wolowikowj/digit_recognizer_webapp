921cd2d2e26f1d630ae31b35ce16c4faca484ba5
jegor <115713987+wolowikowj@users.noreply.github.com>
2024-05-11 16:52:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -91,6 +91,20 @@ with cols[0]:
             # Display the Markdown text
             st.markdown(markdown_text, unsafe_allow_html=True)
 
-expander = st.expander("Explanation")
+with expander("Explanation"):
+     st.markdown(
+          "This is a basic digit recognizer web application. The underlying model is a convolutional neural network that has been trained on the MNIST dataset, with some data augmentation to make it more robust to different handwritings.
 
-expander.write("This is a simple digit recognizer based on a convolutional neural network model that has been trained on the MNIST dataset. I employed some data augmentation to make the model more robust to different handwritings.  \n  \n I did this project to learn more about convolutional neural networks, working with git and deployment in the Streamlit cloud.")
+My goals for this project were:
+
+   * learn more about convolutional networks,
+   * learn to deploy a webapp in streamlit,
+   * learn to work with git
+
+Ideas that could be implented in the future:
+
+   * add options for different background and pen colors and make the webapp/model robust to different contrast levels
+   * set up a pipeline to save drawn images and retrain model with them
+   * make the model more robust to random drawings
+   * show the recognition steps to make the model more transparent"
+     )
